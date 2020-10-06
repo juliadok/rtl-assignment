e77b0797ae8ffe878a6882b512adf3b2a6587246
Julia Dokolina <juliadokolina@gmail.com>
2020-10-06 12:10:08 +0200

    Update App component to fetch articles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import Header from './components/Header';
+import Articles from './components/Articles';
+
 import './App.css';
 
 function App(): React.ReactElement {
-  return <div />;
+  const [error, setError] = useState<Error>();
+  const [isLoading, setLoading] = useState(true);
+  const [articles, setArticles] = useState<Articles>();
+
+  useEffect((): void => {
+    (async (): Promise<void> => {
+      try {
+        const response = await fetch(`${process.env.PUBLIC_URL}/bundle-api.json`);
+        const data = await response.json();
+        setArticles(data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <Alert variant="primary">Loading...</Alert>;
+  }
+
+  if (error) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  if (!articles) {
+    return <Alert variant="danger">No articles</Alert>;
+  }
+
+  return (
+    <>
+      <Header />
+      <Articles articles={articles} />
+    </>
+  );
 }
 
 export default App;
