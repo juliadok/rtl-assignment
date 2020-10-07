98663d2ba952882b96e344c86dce6ea5e5854623
Julia Dokolina (via Travis CI) <juliadokolina@gmail.com>
2020-10-07 07:55:03 +0000

    Deploy 0.1.0

---
+++ precache-manifest.42b70c7e98a67b471e7d579d91268a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3523896a93795344ab02170fdfd5d07f",
+    "url": "/rtl-assignment/index.html"
+  },
+  {
+    "revision": "d708faaed01b99c91868",
+    "url": "/rtl-assignment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8dd8e18003c4f51f1f92",
+    "url": "/rtl-assignment/static/css/main.e4af86f8.chunk.css"
+  },
+  {
+    "revision": "d708faaed01b99c91868",
+    "url": "/rtl-assignment/static/js/2.39973201.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rtl-assignment/static/js/2.39973201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd8e18003c4f51f1f92",
+    "url": "/rtl-assignment/static/js/main.fb9dafee.chunk.js"
+  },
+  {
+    "revision": "67b738beec85e121632d",
+    "url": "/rtl-assignment/static/js/runtime-main.a2a30b9f.js"
+  }
+]);
